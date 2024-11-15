3cb441883a3247c9758b944f82bcf33f710165f3
Jon Person <person.jon@gmail.com>
2024-11-14 21:05:01 -0700

    Upgraded MudBlazor Templates to .NET 9

--- src/mudblazor/MudBlazor.Template.Client/Program.Main.cs
+++ src/mudblazor/MudBlazor.Template.Client/Program.Main.cs
@@ -1,7 +1,3 @@
-#if (IndividualLocalAuth)
-using MudBlazor.Template.Client;
-using Microsoft.AspNetCore.Components.Authorization;
-#endif
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 
@@ -12,16 +8,14 @@ class Program
     static async Task Main(string[] args)
     {
         var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
-        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
         builder.Services.AddMudServices();
 
 #if (IndividualLocalAuth)
         builder.Services.AddAuthorizationCore();
         builder.Services.AddCascadingAuthenticationState();
-        builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
-#endif
+        builder.Services.AddAuthenticationStateDeserialization();
 
+        #endif
         await builder.Build().RunAsync();
     }
 }
